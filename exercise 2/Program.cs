18565d12299b900ec7b0e786d26dc2a7f944d6cf
michail <urafeneks@gmail.com>
2022-08-07 13:02:27 +0300

    LitleChange

--- exercise 2/Program.cs
+++ exercise 2/Program.cs
@@ -11,12 +11,6 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
-if (num2 > max) 
-{
-    max = num2;
-}  
-    if (num3 > max)
-    {
-        max = num3;
-    }
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 Console.WriteLine($"максимальное значение {max}");
